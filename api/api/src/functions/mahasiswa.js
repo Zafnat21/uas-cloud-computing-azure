4ab9be7c64e92d20ad0a4518af107123d3614dc5
Zafnat21 <zafnatpaaneah02@ummi.ac.id>
2026-07-01 23:34:17 +0700

    Add mahasiswa HTTP function
    Implement HTTP function to return mahasiswa data.

---
+++ api/api/src/functions/mahasiswa.js
@@ -0,0 +1,24 @@
+const { app } = require("@azure/functions");
+
+app.http("mahasiswa", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async () => {
+    return {
+      jsonBody: [
+        {
+          nim: "2023001",
+          nama: "Zafnat Paaneah Matusal",
+          prodi: "Teknik Informatika",
+          status: "Aktif"
+        },
+        {
+          nim: "2023002",
+          nama: "Mahasiswa Contoh",
+          prodi: "Sistem Informasi",
+          status: "Aktif"
+        }
+      ]
+    };
+  }
+});
